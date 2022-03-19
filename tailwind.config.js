10d2ff2cd0d413ab7fd42a30e143f5d87daac611
D3vmotiv <czechpiotr2701@gmail.com>
2022-03-19 10:23:49 +0100

    Add project colors&assets config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(40%, 35%, 36%)",
+          light: "hsla(40%, 35%, 36%, 15%)",
+        },
+        secondary: {
+          DEFAULT: "hsl(40%, 12%, 30%)",
+          ligth: "hsla(40%, 12%, 30%, 15%)",
+        },
+        facebookBlue: {
+          DEFAULT: "hsl(208%, 91%, 35%)",
+          ligth: "hsla(208%, 91%, 35%, 15%)",
+        },
+        white: {
+          DEFAULT: "hsl(0%, 0%, 100%)",
+          ligth: "hsl(40%, 13%, 91%)",
+        },
+        black: {
+          DEFAULT: "hsl(0%, 0%, 0%)",
+          ligth: "hsla(0%, 0%, 0%, 15%)",
+        },
+      },
+      fontSize: {
+        sm: "14px",
+        base: "18px",
+        lg: "24px",
+        xl: "30px",
+        "2xl": "50px",
+      },
+      fontFamily: {
+        Limelight: ["Limelight", "sans-serif"],
+        Poppins: ["Poppins", "serif"],
+      },
+    },
   },
   plugins: [],
 };
