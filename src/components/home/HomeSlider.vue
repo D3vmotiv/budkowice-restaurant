639839a6cb4ac90dbb64f371e5601296178d8c65
D3vmotiv <czechpiotr2701@gmail.com>
2022-03-19 10:45:07 +0100

    Init main page

---
+++ src/components/home/HomeSlider.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        id="slider"
+        class="w-screen h-screen flex justify-between items-center p-5"
+    >
+        <div>
+            <slot name="top" />
+        </div>
+
+        <div class="w-full flex">
+            <div>
+
+            </div>
+
+            <div class="w-full md:w-3/4">
+                <slot />
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+
+        <!-- Slider indicators -->
+        <div>
+            <button data-home-slider-step="1" class="slider--indicator"></button>
+            <button data-home-slider-step="2" class="slider--indicator"></button>
+        </div>
+    </div>
+</template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
+import HomeSlider from './../components/home/HomeSlider.vue'
 
 useHead({
-  title: 'Website Title',
-  meta: [
-    {
-      name: `description`,
-      content: `Website description`,
-    },
-  ],
+  title: 'Karczma na górce - Strona główna',
 })
 </script>
 
 <template>
-    <p class="text-3xl font-bold">
-        index.vue
-    </p>
+    <HomeSlider>
+        <div>
+            <h1 class="text-center">
+              Zapraszamy do najlespszej restauracji w okolicy!
+            </h1>
+            <p class="text-center">
+              Pszepyszne dania w dobrej cenie tylko u nas!
+            </p>
+        </div>
+    </HomeSlider>
 </template>
