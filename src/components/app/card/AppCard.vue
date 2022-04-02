af208bf64028721267f7c481a6abd28ece038dbf
D3vmotiv <czechpiotr2701@gmail.com>
2022-04-02 19:01:32 +0200

    Init basic app card

---
+++ src/components/app/card/AppCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    imageUrl: string;
+    sectionName?: string;
+}>();
+
+</script>
+<template>
+    <section
+        class="w-full bg-white"
+        :aria-label="props.sectionName"
+    >
+        <img
+            class="w-full h-80 object-cover"
+            :src="props.imageUrl"
+            alt=""
+        >
+
+        <div class="p-6">
+
+            <slot name="title" />
+
+            <p class="text-base py-3">
+                <slot />
+            </p>
+
+            <slot name="bottom" />
+        </div>
+
+    </section>
+</template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useHead } from "@vueuse/head";
 import HomeSlider from "@/components/home/home-slider/HomeSlider.vue";
-import AppNavigation from '@/components/app/navigation/AppNavigation.vue';
+import AppNavigation from "@/components/app/navigation/AppNavigation.vue";
+import AppCard from "@/components/app/card/AppCard.vue"
 
 useHead({
   title: "Karczma na górce - Strona główna",
@@ -9,9 +10,35 @@ useHead({
 </script>
 
 <template>
-  <HomeSlider>
+  <home-slider>
     <template v-slot:top>
-      <AppNavigation />
+      <app-navigation />
     </template>
-  </HomeSlider>
+  </home-slider>
+
+  <div class="p-5 bg-white-ligth">
+  
+    <app-card
+      image-url="/images/image_1.jpg"
+      section-name="Menu"
+    >
+      <template #title>
+        <h3 class="text-xl mb-2">
+          Poznaj nasze
+          <span class="text-primary">menu</span>
+        </h3>
+      </template>
+
+      <template #default>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales a aliquam et odio bibendum.
+      </template>
+
+      <template #bottom>
+        <!-- todo base-link -->
+        <a href="/menu">Sprawdź nasze menu</a>
+      </template>
+    </app-card>
+
+  </div>
+
 </template>
