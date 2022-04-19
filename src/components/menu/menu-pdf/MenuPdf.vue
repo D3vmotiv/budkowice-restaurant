8b917e8d5602482dda69017d746899cafd792943
D3vmotiv <czechpiotr2701@gmail.com>
2022-04-19 15:52:03 +0200

    Better UX text in menu pdf displayer

--- src/components/menu/menu-pdf/MenuPdf.vue
+++ src/components/menu/menu-pdf/MenuPdf.vue
@@ -9,35 +9,19 @@ const props = defineProps<{
     <object
         :data="props.url"
         type="application/pdf"
-        class="w-full min-h-[70vh] md:min-h-screen"
+        class="flex w-full min-h-[70vh] md:min-h-screen"
     >
-        <div class="p-5">
+        <div class="p-5 h-100 flex flex-col justify-center text-center">
 
-            <h2 class="text-xl mb-3">
-                Wystąpił problem
+            <h2 class="text-xl mb-4">
+                Pobieranie rozpoczęte!
             </h2>
 
             <p>
-                ale nie przejmuj się! <br><br>
-                Przygotowaliśmy dla Ciebie rozwiązanie. <br><br>
-
-                Jeśli chcesz możesz pobrać zawartość klikając 
-
+                Jeśli akcja nie rozpoczeła się automatycznie kliknij
                 <a :href="props.url" download class="text-primary font-bold">tutaj</a>.
 
                 <br><br>
-
-                Alternatywnie możesz odwiedzić
-                <a
-                    href="https://www.facebook.com/Karczma-na-G%C3%B3rce-255581354538501/"
-                    target="_blank"
-                    rel="noopener nofollow"
-                    class="text-facebookBlue font-bold"
-                >
-                naszą stronę na facebooku
-                </a>
-                
-                i tam znaleźć interesujące Cię informacje!
             </p>
         </div>
     </object> 
